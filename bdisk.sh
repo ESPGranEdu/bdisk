0cfc160a4b021f3b4941684a6321ba8bd93693cd
ESPGranEdu <raplhwigum@gmail.com>
2019-04-05 00:48:11 +0200

    Reformating script structure

--- bdisk.sh
+++ bdisk.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Cargar el contenido de la carpet modules
+
+if [ ! -d modules  ]; then
+    echo -e "\e[1;91mNo se encuentra la carpeta \"modules\"\e[0m"
+    exit 1
+elif [ ! -f modules/functions.sh ]; then
+    echo -e "\e[1;91mError al cargar \"functions.sh\"\e[0m"
+    exit 1
+else
+    source modules/*
+fi
 # Comprobamos si el usuario tiene permisos de super usuarios
 
 if [ $(id -u) != 0 ]; then
@@ -9,54 +20,60 @@ fi
 # Primero le pedimos al usuario que introduzca el disco
 # al cual se le va a hacer la copia de segutidad
 
-echo -n "Introduce el disco al que se le quiere hacer una copia --> "; read disco
-
-# Variables y arrays para sacar correctamente la informacion del disco
-
-disco_f=$(echo $disco | cut -f3 -d/)
-num_part=$(lsblk -f $disco | tr -d "├─" | egrep "$disco_f[1-9]" | wc -l)
-mapfile -t part < <(lsblk -f $disco | tr -d "├─" | egrep "$disco_f[1-9]" | awk '{print $1}' )
-mapfile -t tipo_part < <(lsblk -f $disco | tr -d "├─" | egrep "$disco_f[1-9]" | awk '{print $2}')
-
-# Funciones para detectar si el disco proporcinado existe
-# Y crear la tabla de particiones del disco escogido
-
-function buscar_disco(){
-    lsblk $1 &> /dev/null
-
-    if [ $(echo $?) != 0 ]; then
-        echo "El disco no existe, abortando..."
-        exit 1
-    else
-        echo "El disco $1 esta en el sistema"
-    fi
-}
-
-function backup_tabla_particiones(){
-    gdisk $1 << EOF
-    b
-    Tabla_particiones.img
-    w
-EOF
-
-}
+echo -en "\e[1mIntroduce el disco al que se le quiere hacer una copia --> \e[0m"; read -e disco
 
 # Miramos si el disco que ha proporcionado el usuario existe en el equipo
 
-buscar_disco $disco
+comprobar $disco
 
-# Si se encuentra el disco, se realizara la copia de seguridad
-# de la tabla de particiones y de las particiones del disco
+# Variables y arrays para sacar correctamente la informacion del disco
 
-# Backup de la tabla de particiones
+tipo_tabla=$(fdisk -l $disco | grep -oP "dos|gpt")
+disco_f=$(echo $disco | cut -f3 -d/)
+num_part=$(lsblk -lf $disco | egrep "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | wc -l)
+mapfile -t part < <(lsblk -lf $disco | egrep "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $1}' )
+mapfile -t tipo_part < <(lsblk -lf $disco | egrep "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $2}')
 
-backup_tabla_particiones $disco &> /dev/null
+# Comprobamos si ya existe una copia de la tabla de particiones
+# para evitar operaciones de escritura y lectura si a lo que
+# se le va a hacer una copia de seguridad es un disco SSD
 
-# Backup de las particiones
+crear_backup $disco $disco_f $tipo_tabla
 
-for X in $(seq 0 $(($num_part-1)));
-do 
-    echo "partclone.${tipo_part[$X]} -Ncs /dev/${part[$X]} | gzip -c > ${part[$X]}.pc.gz"
+# Antes de realizar el backup de las particiones, le mostramos al usuario las particiones
+# a las que se le va a hacer una copia de seguridad
+
+echo -e "\e[1;92mSe van a realizar las siguientes copias: \e[0m"
+echo ""
+
+# Le mostramos las particiones a las que se le va a hacer una copia de seguridad
+# y el tipo de la particion, si no se conoce el formato, se muestra "desconocido"
+
+for disk in $(seq 0 $(($num_part-1)));
+do
+    if [ "${tipo_part[$disk]}" == "" ]; then
+        tipo_part[$disk]="desconocido"
+    fi
+
+    echo -e "\e[1;93m/dev/${part[$disk]}\e[0m \e[1mde tipo\e[0m \e[1;96m${tipo_part[$disk]}\e[0m"
 done
 
-exit 0
+# Despues de mostrarle las particiones al usuario, le preguntamos al usuario si
+# desea realizar la copia de estas particiones
+
+echo ""
+echo -en "\e[1mDesea realizar la copia de las siguientes particiones[S/n]: \e[0m"; read user
+
+if [[ "$user" == "S" || "$user" == "s" || "$user" == "" ]]; then
+    # Backup de las particiones
+    
+    for X in $(seq 0 $(($num_part-1)));
+    do
+        partclone.${tipo_part[$X]} -Ncs /dev/${part[$X]} | gzip -c > ${part[$X]}.pc.gz
+    done
+    
+    exit 0
+elif [[ "$user" == "N" || "$user" == "n" ]]; then
+    echo -e "\e[1;91mAbortando...\e[0m"
+    exit 1
+fi
---
+++ modules/functions.sh
@@ -0,0 +1,23 @@
+function comprobar() {
+    lsblk $1 &> /dev/null
+    
+    if [ $(echo $?) != 0 ]; then
+        echo -e "\e[1;91mEl disco no existe, abortando...\e[0m"
+        exit 1
+    fi
+    
+}
+
+function crear_backup() {
+    if [ -f "Tabla_particiones_$2.img" ]; then
+        echo -e "\e[1;33mYa existe la tabla de particiones del disco $1\e[0m"
+        
+    else
+        if [ "$3" == "gpt" ]; then
+            dd if=$1 of="Tabla_particiones_$2.img" bs=512 count=34 &> /dev/null
+        elif [ "$3" == "dos" ]; then
+            dd if=$1 of="Tabla_particiones_$2.img" bs=512 count=1 &> /dev/null
+        fi
+    fi
+        echo -e "\e[1;92mCreada copia de la tabla de particiones del disco\e[0m \e[1;93m$1\e[0m"
+}
