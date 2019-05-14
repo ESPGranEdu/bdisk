1e4b0d965c6fdc57fbab2c89fdef02b08903d5ed
ESPGranEdu <raplhwigum@gmail.com>
2019-05-14 18:10:06 +0200

    Reformating script

--- bdisk.sh
+++ bdisk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Limpiamos el contenido de la terminal actual para mayor visualizacion 
+# Limpiamos el contenido de la terminal actual para mayor visualizacion
 
 clear
 
@@ -8,11 +8,11 @@ clear
 if [ ! -d modules  ]; then
     echo -e "\e[1;91mNo se encuentra la carpeta \"modules\"\e[0m"
     exit 1
-elif [ ! -f modules/functions.sh ]; then
+    elif [ ! -f modules/functions.sh ]; then
     echo -e "\e[1;91mError al cargar \"functions.sh\"\e[0m"
-
+    
     exit 1
-elif [ ! -f modules/check_dependencies.sh ]; then
+    elif [ ! -f modules/check_dependencies.sh ]; then
     echo -e "\e[1;91mError al cargar \"check_dependencies.sh\"\e[0m"
     exit 1
 else
@@ -27,12 +27,12 @@ fi
 if [ "$(id -u)" != 0 ]; then
     echo "Debes de tener permisos de administrador para ejecutar este script"
     exit 1
-fi 
+fi
 
 # Primero le pedimos al usuario que introduzca el disco
 # al cual se le va a hacer la copia de seguridad
 
-disco=$(fdisk -l | grep -E "/dev/sd[a-z]:" | awk '{print $2,$3,$4}' | tr ":" "\t" | sed 's/,$//' | fzy --prompt="Selecciona el disco al que quieres hacerle un backup --> " | awk '{print $1}') 
+disco=$(fdisk -l | grep -E "/dev/sd[a-z]:|/dev/nvme0n[1-9]" | awk '{print $2,$3,$4}' | tr ":" "\t" | sed 's/,$//' | fzf --reverse --prompt="Selecciona el disco al que quieres hacerle un backup --> " | awk '{print $1}')
 
 # Miramos si el disco que ha proporcionado el usuario existe en el equipo
 
@@ -43,15 +43,10 @@ comprobar $disco
 aviso_montaje=0
 tipo_tabla=$(fdisk -l $disco | grep -oP "dos|gpt")
 disco_f=$(echo $disco | cut -f3 -d/)
-num_part=$(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | wc -l)
-mapfile -t part < <(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $1}' )
-mapfile -t tipo_part < <(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $2}')
+num_part=$(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])|${disco_f}p([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | wc -l)
+mapfile -t part < <(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])|${disco_f}p([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $1}' )
+mapfile -t tipo_part < <(lsblk -lf $disco | grep -E "$disco_f([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])|${disco_f}p([1-9]|[1-8][0-9]|9[0-9]|1[01][0-9]|12[0-8])" | awk '{print $2}')
 
-# Comprobamos si ya existe una copia de la tabla de particiones
-# para evitar operaciones de escritura y lectura si a lo que
-# se le va a hacer una copia de seguridad es un disco SSD
-
- crear_backup $disco $disco_f $tipo_tabla
 
 # Antes de realizar el backup de las particiones, le mostramos al usuario las particiones
 # a las que se le va a hacer una copia de seguridad
@@ -68,43 +63,58 @@ do
         tipo_part[$disk]="desconocido"
     fi
     
-    # Aqui se comprueba si la particion esta montada o no, debido a que si esta montada, partclone no podra 
+    # Aqui se comprueba si la particion esta montada o no, debido a que si esta montada, partclone no podra
     # realizar correctamente la copia de seguridad del disco duro por este motivo
-
+    
     if grep -q ${part[$disk]} /proc/mounts; then
-    	echo -e "\e[1;93m/dev/${part[$disk]}\e[0m \e[1mde tipo\e[0m \e[1;96m${tipo_part[$disk]}\e[0m \e[1;91mMONTADO\e[0m"
+        echo -e "\e[1;93m/dev/${part[$disk]}\e[0m \e[1mde tipo\e[0m \e[1;96m${tipo_part[$disk]}\e[0m \e[1;91mMONTADO\e[0m"
         aviso_montaje=1
-	else
-    	echo -e "\e[1;93m/dev/${part[$disk]}\e[0m \e[1mde tipo\e[0m \e[1;96m${tipo_part[$disk]}\e[0m"
+    else
+        echo -e "\e[1;93m/dev/${part[$disk]}\e[0m \e[1mde tipo\e[0m \e[1;96m${tipo_part[$disk]}\e[0m"
     fi
-
+    
 done
 
 # Despues de mostrarle las particiones al usuario, le preguntamos al usuario si
 # desea realizar la copia de estas particiones, pero si se ha detectado alguna
-# particion montada, se le pregunta al usuario si desea desmontarla para hacer 
+# particion montada, se le pregunta al usuario si desea desmontarla para hacer
 # copia, si no, se hacen las copias pero no de las particiones que estan montadas
 
 echo ""
 
 if [ $aviso_montaje != 0 ]; then
     echo -en "\e[1;95mSe han detectado particiones montadas, si se desea realizar la copia de seguridad
-se van a desmontar las particiones. ¿desea proseguir?[S/n]: \e[0m"; read user
+    se van a desmontar las particiones. ¿desea proseguir?[S/n]: \e[0m"; read user
     if [[ "$user" == "S" || "$user" == "s" || "$user" == "" ]]; then
         
+        dir_user=$(find /home /mnt /run/media /media -type d 2>/dev/null | fzf --reverse --prompt "Escoge un directorio para guardar las copias, si no existe se creara --> ")_$(date -I)
+        
+        if [ -d $dir_user ]; then
+            cd $dir_user
+            
+            # Comprobamos si ya existe una copia de la tabla de particiones
+            # para evitar operaciones de escritura y lectura si a lo que
+            # se le va a hacer una copia de seguridad es un disco SSD
+            
+            crear_backup $disco $disco_f $tipo_tabla
+        else
+            mkdir- p $dir_user
+            cd $dir_user
+            crear_backup $disco $disco_f $tipo_tabla
+        fi
         # Desmontamos las particiones que esten montadas
-
+        
         for disk in $(seq 0 $((num_part-1)));
         do
             if grep -q ${part[$disk]} /proc/mounts; then
-                umount "/dev/${part[$disk]}"	
+                umount "/dev/${part[$disk]}"
             fi
         done
-	
         
-      
+        
+        
         # Hacemos la copia de seguridad
-
+        
         for X in $(seq 0 $((num_part-1)));
         do
             if [ "${tipo_part[$X]}" == "desconocido" ]; then
@@ -113,36 +123,26 @@ se van a desmontar las particiones. ¿desea proseguir?[S/n]: \e[0m"; read user
                 partclone.${tipo_part[$X]} -Ncs /dev/${part[$X]} | gzip -c > ${part[$X]}.pc.gz
             fi
         done
-    
-    	# Ahora depues de realizar la copia de seguridadm le mostramos al usuario un buscador para guardar las copias en el directorio deseado
-    	# y si no existe el directorio que se busca, se creara automaticamente y se moveran los archivos alli; se concatenara la fecha 
-
-    	dir_user=$(find /home /mnt /run/media /media -type d 2>/dev/null | fzy --prompt "Escoge un directorio para guardar las copias, si no existe se creara --> ")_$(date -I)
-
-		if [ -d $dir_user ]; then
-			rsync -r --progress *.gz *.img $dir_user
-			echo -e "\e[1;96mSincronizando...\e[0;"
-			sync
-			rm -rf *.gz *.img
-		else
-			mkdir -p $dir_user
-			rsync -r --progress *.gz *.img $dir_user
-			echo -e "\e[1;96mSincronizando...\e[0m"
-			sync
-			rm -rf *.gz *.img
-	    fi
-	        exit 0
-	else  
+        exit 0
+    else
         echo -e "\e[1;91mAbortando...\e[0m"
-        exit 1        
+        exit 1
     fi
 fi
 
 echo -en "\e[1m¿Desea realizar la copia de las siguientes particiones? [S/n]: \e[0m"; read user
+dir_user=$(find /home /mnt /run/media /media -type d 2>/dev/null | fzf --reverse --prompt "Escoge un directorio para guardar las copias, si no existe se creara --> ")_$(date -I)
 
-if [[ "$user" == "S" || "$user" == "s" || "$user" == "" ]]; then
+if [ -d $dir_user ]; then
+    cd $dir_user            
+    crear_backup $disco $disco_f $tipo_tabla
+else
+    mkdir- p $dir_user
+    cd $dir_user
+    crear_backup $disco $disco_f $tipo_tabla
+fi
     # Backup de las particiones
-
+    
     for X in $(seq 0 $((num_part-1)));
     do
         if [ "${tipo_part[$X]}" == "desconocido" ]; then
@@ -151,24 +151,9 @@ if [[ "$user" == "S" || "$user" == "s" || "$user" == "" ]]; then
             partclone.${tipo_part[$X]} -Ncs /dev/${part[$X]} | gzip -c > ${part[$X]}.pc.gz
         fi
     done
-
-	dir_user=$(find /home /mnt /run/media /media -type d 2>/dev/null | fzy --prompt "Escoge un directorio para guardar las copias, si no existe se creara --> ")_$(date -I)
-
-	if [ -d $dir_user ]; then
-		rsync -r --progress *.gz *.img $dir_user
-		echo -e "\e[1;96mSincronizando...\e[0;"
-		sync
-		rm -rf *.gz *.img
-	else
-		mkdir -p $dir_user
-		rsync -r --progress *.gz *.img $dir_user
-		echo -e "\e[1;96mSincronizando...\e[0m"
-		sync
-		rm -rf *.gz *.img
-    fi
-
+    
     exit 0
-
+    
 else
     echo -e "\e[1;91mAbortando...\e[0m"
     exit 1
